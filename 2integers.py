92efa570c4915f5edd8ff2c13f0d73c3a381f913
goreshitcore <165133434+goreshitcore@users.noreply.github.com>
2024-07-30 08:17:02 +1200

    Create 2integers.py

---
+++ 2integers.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first integer N:"))
+print(f"{a}")
+b = int(input("Enter the second integer N:"))
+print(f"{b}")
+product = a * b
+print(f"The product of {a} and {b} is {product}.")
