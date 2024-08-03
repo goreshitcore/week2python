a69a16a0118c4cd1cdd986399815461f03d695c3
goreshitcore <165133434+goreshitcore@users.noreply.github.com>
2024-08-04 11:31:09 +1200

    Create roundarray.py

---
+++ roundarray.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# Original array
+original_array = np.array([4.0, 0.056, 16.991, 36.5])
+print(f"Original Array: {original_array}")
+print(f"Two Decimal Element-wise Rounding: {np.round(original_array, 2)}")
